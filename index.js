87dc860a7a138f1da66fa65acf2e471463bc5649
rosangela <146039254+rosangelahb@users.noreply.github.com>
2023-11-05 20:09:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-// Variáveis
 
-let nome = "Tanjiro"; // Substitua pelo nome do herói
-let XP = 9001; // Substitua pela quantidade de XP do herói
+![Captura de Tela (8842)](https://github.com/LillyButterfly/Classificador-de-Nivel-de-Heroi/assets/88951197/4d9a153f-781e-45a6-b0db-858322006ea0)
 
-// Estrutura de decisão para classificar o nível
-let nivel;
-if (XP <= 1000) {
- nivel = "Ferro";
-} else if (XP >= 1001 && XP <= 2000) {
- nivel = "Bronze";
-} else if (XP >= 2001 && XP <= 5000) {
- nivel = "Prata";
-} else if (XP >= 5001 && XP <= 6000) {
- nivel = "Aço";
-} else if (XP >= 6001 && XP <= 7000) {
- nivel = "Ouro";
-} else if (XP >= 7001 && XP <= 8000) {
- nivel = "Platina";
-} else if (XP >= 8001 && XP <= 9000) {
- nivel = "Ascendente";
-} else if (XP >= 9001 && XP <= 10000) {
- nivel = "Imortal";
-} else {
- nivel = "Radiante";
-}
-// Exibir a mensagem
-console.log("O Herói " + nome + " está no nível: " + nivel)
+
+## Tecnologias
+
+- HTML e CSS
+- JavaScript
+
+
+Instruções para entrega
+# 1️⃣ Desafio Classificador de nível de Herói
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 6.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"
